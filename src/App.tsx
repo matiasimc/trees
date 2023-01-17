ce880d2cf00f75d40b9b4c9d6d4ae91c67656a00
Matias Meneses <matias.imc@gmail.com>
2023-01-17 00:20:46 -0300

    Make trees state a record type instead of JSX.Element

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,33 @@ import TreeView from "./components/tree-view/TreeView";
 import { useEffect, useState } from "react";
 import "./App.css";
 
+type Tree = {name: string, species_name: string, image: string};
+
 function App() {
-  const [trees, setTrees] = useState<JSX.Element[]>([]);
+  const [trees, setTrees] = useState<Tree[]>([]);
 
   useEffect(() => {
     fetch(
       "https://s3.eu-central-1.amazonaws.com/ecosia-frontend-developer/trees.json"
     )
       .then((response) => response.json())
-      .then(buildTreeViews)
+      .then((json) => json["trees"])
       .then(setTrees);
   }, []);
 
   return (
     <div className="App">
-      <Gallery>{trees}</Gallery>
+      <Gallery>{buildTreeViews(trees)}</Gallery>
     </div>
   );
 }
 
-function buildTreeViews(
-  json: Record<string, Record<string, string>[]>
-): JSX.Element[] {
-  return json["trees"].map((tree, index) => (
+function buildTreeViews(trees: Tree[]): JSX.Element[] {
+  return trees.map((tree, index) => (
     <TreeView
-      name={tree["name"]}
-      species_name={tree["species_name"]}
-      image={tree["image"]}
+      name={tree.name}
+      species_name={tree.species_name}
+      image={tree.image}
       key={index}
     ></TreeView>
   ));
